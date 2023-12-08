4fd498aca8bd47a7949e841035a879c8b1a55584
alyhenr <alyssonhra99@gmail.com>
2023-12-08 12:36:52 -0300

    db-seed: seeding data to db when starting api

---
+++ Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,35 @@
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly StoreContext _context;
+        public ProductRepository(StoreContext context)
+        {
+            _context = context;
+        }
+
+        public Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Product> GetProductByIdASync(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}
